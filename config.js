1e8309ff7b1905c22b5a1302c12299883ffa01ec
UP2010416 <91854274+UP2010416@users.noreply.github.com>
2023-07-11 02:29:40 +0100

    lint scripts added to package.json

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 export default {
-  // remove the next line if not running on our VM and connection to PostgreSQL doesn't work
-  // host: '/var/run/postgresql',
-
   database: 'inventoryapp',
   statement_timeout: 5000,
 };
